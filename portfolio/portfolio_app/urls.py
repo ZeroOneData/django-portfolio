89c72f4000c61091bd08fa7401bfad93f59bc33d
unknown <tyronanderson05@gmail.com>
2022-04-20 20:00:54 +0200

    Seventh commit - Implemented interactive folium map page and functionality. The map renders the location of each user Profile as a marker on the map - whose onClick signal opens up that specific user's Profile dialog.

--- portfolio/portfolio_app/urls.py
+++ portfolio/portfolio_app/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path
 #from . import views
-from .views import HomeView, MapView, ProfileDetailView
+from .views import HomeView,  ProfileDetailView, mapView
 
 
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name = "home" ),
-    path('map/', MapView.as_view(), name="map"),
+    path('map/', mapView, name="map"),
     path('profiles/<int:pk>', ProfileDetailView.as_view(), name = "profile_detail"),
 ]
--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,8 +1,11 @@
 from re import template
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Profile
 from django.views.generic import TemplateView
+import folium
+import geocoder
 
 #temp import
 from django.contrib.auth.models import User
@@ -21,5 +24,56 @@ class ProfileDetailView(DetailView):
     model = User
     template_name = 'profile_details.html'
 
-class MapView(TemplateView):
-    template_name = 'map.html'
+def mapView(request):
+
+            # Create Map Object
+    m = folium.Map(location=[19, -12], zoom_start=2)
+
+    # address = Profile.objects.all().last()
+    addresses = Profile.objects.all()
+    for address in addresses:
+        print(address.home_address)
+
+        location = geocoder.osm(address.home_address)
+        lat = location.lat
+        lng = location.lng
+        country = location.country
+        if lat == None or lng == None:
+            return HttpResponse('You address input is invalid')
+        
+        def popup_html(address, country):
+            popCity = address.home_address
+            popPhone = address.phone_number
+            popFirstName = address.user.first_name
+            popLastName = address.user.last_name
+            
+            html = """<!DOCTYPE html>
+                <html>
+                    <div class="shadow mb-5 bg-body rounded" style="max-width: 540px;">
+                        <div class="card mb-3" style="max-width: 540px;">
+                            <div class="row g-0">  
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">"""+ popFirstName  + ' ' + popLastName +"""</h5>
+                                        <p class="card-text">My City: """+ popCity +"""</p>
+                                        <p class="card-text">Country: """+ country +"""</p>
+                                        <p class="card-text"><small class="text-muted">Phone number: """+ popPhone +"""</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </html>
+                """
+            return html
+
+        popup = folium.Popup(folium.Html(popup_html(address, country), script=True), max_width=500)
+        folium.Marker([lat, lng], tooltip=location.city,
+                    popup=popup).add_to(m)
+
+    # Get HTML Representation of Map Object
+    m = m._repr_html_()
+    context = {
+        'm': m,
+    }
+    return render(request, 'map.html', context)
